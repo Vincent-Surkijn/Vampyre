9586b4fe985d441c1e0a3dce4847877329e7164d
Vincent S <vincent.surkijn@student.kuleuven.be>
2019-05-23 12:46:34 +0200

    Aantal knoppen in main terug een functie gegeven In de score shop wordt nu het aantal tokens weergegeven en als er op een product geduwd wordt, gaat het aantal tokens dat het kost er van af.

--- app/src/main/java/vincentsurkijn/softdev/kuleuven/vampyre/Score_shop.java
+++ app/src/main/java/vincentsurkijn/softdev/kuleuven/vampyre/Score_shop.java
@@ -2,12 +2,109 @@ package vincentsurkijn.softdev.kuleuven.vampyre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Score_shop extends AppCompatActivity {
+
+    private TextView AmountOfTickets_text;
+    private int AmountOfTickets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_shop);
+        AmountOfTickets_text = findViewById(R.id.AmountOfTokens_textShop);
+        setAmountOfTickets();
+    }
+
+    public void setAmountOfTickets(){
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        String url = "https://studev.groept.be/api/a18_sd209/APP_getAmountOfTokens/"+Login.user;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONObject Tokens = response.getJSONObject(0);
+                            AmountOfTickets = Tokens.getInt("tokens");
+                            String TicketsText = "Your amount of Tickets: " + String.valueOf(AmountOfTickets);
+                            AmountOfTickets_text.setText(TicketsText);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                , new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        });
+        mQueue.add(request);
+    }
+
+    public void oneTicketBuy(View v){
+        ticketBuy(1);
+    }
+
+    public void twoTicketBuy(View v){
+        ticketBuy(2);
+    }
+
+    public void threeTicketBuy(View v){
+        ticketBuy(3);
+    }
+
+    public void fourTicketBuy(View v){
+        ticketBuy(4);
+    }
+
+    public void sixTicketBuy(View v){
+        ticketBuy(6);
+    }
+
+    public void sixteenTicketBuy(View v){
+        ticketBuy(16);
+    }
+
+    public void twentyTicketBuy(View v){
+        ticketBuy(20);
+    }
+
+    public void ticketBuy(int amount){
+        if (AmountOfTickets>0){
+            AmountOfTickets-=amount;
+        }
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        String url = "https://studev.groept.be/api/a18_sd209/APP_setAmountOfTokens/"+ AmountOfTickets +"/" +Login.user;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                       String TicketsText = "Your amount of Tickets: " + String.valueOf(AmountOfTickets);
+                       AmountOfTickets_text.setText(TicketsText);
+                    }
+                }
+                , new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        });
+        mQueue.add(request);
     }
 }
